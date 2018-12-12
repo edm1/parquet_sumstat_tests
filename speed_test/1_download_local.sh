9c99d5b05fa4fcb104775c67c07ef0141c2a7817
edm1 <emountjoy@gmail.com>
2018-12-12 11:45:50 +0000

    use remote

--- speed_test/1_download_local.sh
+++ speed_test/1_download_local.sh
@@ -5,7 +5,7 @@ set -euo pipefail
 
 mkdir -p local
 
-gsutil -m cp -r gs://genetics-portal-sumstats/test/GTEX7 local/GTEX7
+gsutil -m cp -r gs://genetics-portal-sumstats/test/GTEX7 local
 
 # Alt method
 # cd local
